cab14ba11f1c77a59ca4b45265b294df41529c22
Leonardo8221 <r.leonardo0802@gmail.com>
2022-04-28 18:03:48 -0500

    chore: set up mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { FC } from 'react';
+import { ThemeProvider, CssBaseline, Button } from '@mui/material';
+
+import theme from 'core/theme';
 
 const App: FC = () => {
   return (
-    <div>
-      <h1>CustomerCity</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div>
+        <h1>CustomerCity</h1>
+        <Button>Click me!</Button>
+      </div>
+    </ThemeProvider>
   );
 };
 
