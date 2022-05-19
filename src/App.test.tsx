7ba83c89ed106202eb6d1cabc2d385fc0f6341f5
Leonardo8221 <r.leonardo0802@gmail.com>
2022-05-19 18:14:35 -0500

    chore: add test utils and update app test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the connected app without crashing', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByRole('heading', { name: /welcome back/i })).toBeInTheDocument();
 });
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+window.matchMedia =
+  window.matchMedia ||
+  (() => {
+    return { matches: false, addListener: jest.fn(), removeListener: jest.fn() };
+  });
+
+window.scrollTo = jest.fn();
