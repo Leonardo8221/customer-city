0048339a8c21dfb34f708453ef07b23e0348632f
Leonardo8221 <r.leonardo0802@gmail.com>
2022-07-12 21:50:26 -0400

    update linklist

--- src/pages/IntegrationDetail/IntegrationDetail.tsx
+++ src/pages/IntegrationDetail/IntegrationDetail.tsx
@@ -1,15 +1,19 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { Container } from './ui';
 
 import { useParams } from 'react-router-dom';
 import { IntegrationProperty, IntegrationDetail } from './components';
+import { useIntegration } from 'store/integration/hooks';
 
 const IntegrationDetailPage: FC = () => {
   const { id: appId } = useParams();
-
+  const { getIntegration, integration } = useIntegration();
+  useEffect(() => {
+    getIntegration(String(appId));
+  }, [getIntegration, appId, integration]);
   return (
     <Container>
-      <IntegrationProperty appId={String(appId)} />
+      <IntegrationProperty {...integration} />
       <IntegrationDetail />
     </Container>
   );
