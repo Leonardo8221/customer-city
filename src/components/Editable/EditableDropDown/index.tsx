4a51eb2b711b6edbf8b383e9bc680f0be444adf9
Leonardo8221 <r.leonardo0802@gmail.com>
2022-07-30 05:18:41 +0300

    fix clickaway

--- src/components/Editable/EditableDropDown/index.tsx
+++ src/components/Editable/EditableDropDown/index.tsx
@@ -1,5 +1,5 @@
 import { FC, useState } from 'react';
-import { OutlinedTextFieldProps, Box, Typography, Paper } from '@mui/material';
+import { OutlinedTextFieldProps, Box, Typography, Paper, ClickAwayListener } from '@mui/material';
 
 import { ReactComponent as EditIcon } from 'assets/icons/edit.svg';
 import { ReactComponent as AccountIcon } from 'assets/icons/accountIcon.svg';
@@ -30,7 +30,7 @@ const EditableAutoComplete: FC<EditableAutoCompleteProps> = ({
   options = [],
   onSave,
 }) => {
-  const [editing, setEditing] = useState(false);
+  const [editing, setEditing] = useState<boolean>(false);
   // const [inputValue, setInputValue] = useState<OptionValue<number> | null>();
 
   const onClose = async (inputValue: number) => {
@@ -47,29 +47,31 @@ const EditableAutoComplete: FC<EditableAutoCompleteProps> = ({
 
   if (editing) {
     return (
-      <Box position="relative">
-        <CustomDropdown<number>
-          id={id}
-          label={label}
-          placeholder={`Select the ${label}`}
-          value={value || null}
-          options={options}
-          onSelect={(value) => {
-            onClose(value);
-          }}
-          InputProps={{
-            startAdornment: icon ? (
-              <Box display="flex" justifyContent="center" alignItems="center" marginLeft="6px" marginRight="3px">
-                {icon === 'account' && <AccountIcon />}
-                {icon === 'contact' && <ContactIcon />}
-                {icon === 'deal' && <DealIcon />}
-              </Box>
-            ) : null,
-            onBlur: () => onClose,
-          }}
-          PaperComponent={Paper}
-        />
-      </Box>
+      <ClickAwayListener onClickAway={() => setEditing(false)}>
+        <Box position="relative">
+          <CustomDropdown<number>
+            id={id}
+            label={label}
+            placeholder={`Select the ${label}`}
+            value={value || null}
+            options={options}
+            onSelect={(value) => {
+              onClose(value);
+            }}
+            InputProps={{
+              startAdornment: icon ? (
+                <Box display="flex" justifyContent="center" alignItems="center" marginLeft="6px" marginRight="3px">
+                  {icon === 'account' && <AccountIcon />}
+                  {icon === 'contact' && <ContactIcon />}
+                  {icon === 'deal' && <DealIcon />}
+                </Box>
+              ) : null,
+              onBlur: () => onClose,
+            }}
+            PaperComponent={Paper}
+          />
+        </Box>
+      </ClickAwayListener>
     );
   }
 
